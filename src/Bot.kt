d982b6dc2596b29edb6f04947034c303134bce14
onebone <jyc00410@gmail.com>
2018-06-18 16:10:23 +0000

    Implemented basic bot

---
+++ src/Bot.kt
@@ -0,0 +1,32 @@
+package me.onebone.tgbot
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.api.objects.Update
+import org.telegram.telegrambots.api.methods.send.SendMessage
+import org.telegram.telegrambots.exceptions.TelegramApiException
+
+class Bot(
+		private val token: String
+): TelegramLongPollingBot() {
+	private val username: String
+
+	init {
+		username = getMe().userName
+	}
+
+	override fun onUpdateReceived(update: Update) {
+		if(update.hasMessage() && update.getMessage().hasText()){
+			val msg = SendMessage()
+					.setChatId(update.getMessage().getChatId())
+					.setText("Hello World")
+
+			try{
+				execute(msg)
+			}catch(e: TelegramApiException){}
+		}
+	}
+
+	override fun getBotUsername(): String = username
+
+	override fun getBotToken(): String = token
+}
--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,37 @@
 package me.onebone.tgbot
 
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.TelegramBotsApi
+import org.telegram.telegrambots.exceptions.TelegramApiException
+import java.io.File
+import java.io.FileOutputStream
+import com.beust.klaxon.Klaxon
+import com.beust.klaxon.Json
+
 fun main(args: Array<String>) {
-	
+	if(!File("config.json").exists()) {
+		Object::class.java.getResourceAsStream("/config.json").use {input ->
+			FileOutputStream(File("config.json")).use {
+				input.copyTo(it)
+			}
+		}
+	}
+
+	val config = Klaxon().parse<Config>(File("config.json"))!!
+
+
+	ApiContextInitializer.init()
+
+	val api = TelegramBotsApi()
+
+	try{
+		api.registerBot(Bot(config.token))
+	}catch(e: TelegramApiException){
+		e.printStackTrace()
+	}
 }
+
+data class Config (
+	@Json(name="token")
+	val token: String
+)
